c498bd5235498a2852f8f64730bd7a6cb4cbac9a
DESKTOP-IHIDJ8G\PC <mucahitkalabalik@outlook.com>
2025-05-14 00:44:27 +0300

    .

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
-  },
-  reactStrictMode: false, // React Strict Mode'u devre dışı bırakıyoruz
-};
-
-export default nextConfig;
